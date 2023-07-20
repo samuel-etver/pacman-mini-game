287c9b4ebb5d5c41c4239c6b0772f6d2b1431dec
Samuel Etver <samuel.etver@gmail.com>
2023-07-20 23:28:25 +0500

    update tileset

---
+++ arts/Maps/layer1-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="layer1-tiles" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="layer1-tiles.png" width="768" height="768"/>
+</tileset>
---
+++ assets/Maps/layer2-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="layer2-tiles" tilewidth="32" tileheight="32" tilecount="1250" columns="50">
+ <image source="layer2-tiles.png" width="1600" height="800"/>
+</tileset>
